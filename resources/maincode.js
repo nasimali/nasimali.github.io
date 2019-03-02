8b75d0af4b393913152f762d919bef59c55d2ed2
nasimali <nasim133@icloud.com>
2019-03-02 00:58:18 +0000

    Added bootstrap tooltips

--- resources/maincode.js
+++ resources/maincode.js
@@ -1,4 +1,12 @@
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
 function openCv(){
   window.open("resources/profile.pdf");
+}
+
+function openEmail(){
+  window.location.href = "mailto:nma@nasim.dev";
 }
