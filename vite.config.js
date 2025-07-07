85b7726feff11bfe3b4e669c6ca304ebd5a6381f
nasimali <22627654+nasimali@users.noreply.github.com>
2025-07-07 01:59:35 +0100

    feat(2.5): updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-oxc';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import compression from 'vite-plugin-compression';
+import Inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: true,
+    }),
+    Inspect(),
+  ],
   build: {
     chunkSizeWarningLimit: 2000,
+    target: 'es2020',
+    minify: 'esbuild',
+    sourcemap: false,
   },
 });
